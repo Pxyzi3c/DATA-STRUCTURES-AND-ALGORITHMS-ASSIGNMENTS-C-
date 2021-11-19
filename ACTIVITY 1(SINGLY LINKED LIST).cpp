0a523b1e83ba0997bf054926fe2d52c8a8f2ae6b
Pxyzi3c <72428021+Pxyzi3c@users.noreply.github.com>
2021-11-19 12:01:25 +0800

    Add files via upload

---
+++ ACTIVITY 1(SINGLY LINKED LIST).cpp
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+
+void insertAtEnd(Node** head_ref, int new_data)
+{
+    Node* new_node = new Node();
+    Node *last = *head_ref;
+
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    last->next = new_node;
+    return;
+}
+
+void insertAtBeginning(Node** head_ref, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void insertAfter(Node* prev_node, int new_data)
+{
+    if (prev_node == NULL)
+    {
+        cout<<"The given previous node cannot be NULL";
+        return;
+    }
+
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout<<" "<<node->data;
+        node = node->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    insertAtEnd(&head, 1);
+    insertAtBeginning(&head, 2);
+    insertAtBeginning(&head, 3);
+    insertAtEnd(&head, 4);
+    insertAfter(head->next, 5);
+    cout<<"Created Linked list is: ";
+    printList(head);
+
+    return 0;
+}
